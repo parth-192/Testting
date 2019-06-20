5b57469089e2ac545f14ea6ca3732ae1bd4650e5
parth-192 <pate1543@sheridancollege.ca>
2019-06-20 11:05:21 -0400

    student

--- src/cardthursday/Cardthursday.java
+++ src/cardthursday/Cardthursday.java
@@ -2,6 +2,7 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
+Parth Patel 991541548
  */
 package cardthursday;
 
